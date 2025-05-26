9018b621f3392022e1fdb7a23b31f8785884356b
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-05-25 23:12:37 -0400

    [CLASE 7] Creación de Datos Simulados para Proyectos Reales en PHP

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,10 +2,10 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Comment;
-use App\Entity\Metadata;
-use App\Entity\Product;
-use App\Entity\Tag;
+use App\Factory\CommentFactory;
+use App\Factory\ProductFactory;
+use App\Factory\TagFactory;
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -13,45 +13,11 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $product = new Product();
-        $product->setName('Producto de prueba');
-        $product->setSummary('Resumen del producto');
-     
-        $metadata = new Metadata();
-        $metadata->setCode(rand(100, 200));
-        $metadata->setContent('Contenido oficial de producto');
-
-        $manager->persist($metadata);
-
-        $product->setMetadata($metadata);
-        
-        $manager->persist($product);
-
-        // Etiquetas
-        $tag_1 = new Tag();
-        $tag_1->setName('Etiquete # 1');
-        $manager->persist($tag_1);
-        
-        $tag_2 = new Tag();
-        $tag_2->setName('Etiquete # 1');
-        $manager->persist($tag_2);
-
-        $product->addTag($tag_1);
-        $product->addTag($tag_2);
-
-        // Comentarios
-        $comment_1 = new Comment();
-        $comment_1->setContent('Comentario # 1');
-        $manager->persist($comment_1);
-        
-        $comment_2 = new Comment();
-        $comment_2->setContent('Comentario # 2');
-        $manager->persist($comment_2);
-
-        $product->addComment($comment_1);
-        $product->addComment($comment_2);
-
-        // Guardar
-        $manager->flush();
+        TagFactory::createMany(5);
+        ProductFactory::createMany(20, [
+            'comments'  => CommentFactory::new()->many(2, 5),
+            'tags'      => TagFactory::new()->randomRange(2, 5),
+            // 'tags'      => TagFactory::new()->randomSet(2),
+        ]);
     }
 }
